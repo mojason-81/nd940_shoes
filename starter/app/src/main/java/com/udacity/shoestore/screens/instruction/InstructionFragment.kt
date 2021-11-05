afcb833c48971bad6dcb89bc7eeb9078e6304516
Jason Force <jason.r.force@gmail.com>
2021-11-05 15:55:42 -0500

    More nav, strings, labels

--- starter/app/src/main/java/com/udacity/shoestore/screens/instruction/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/instruction/InstructionFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
@@ -18,6 +19,9 @@ class InstructionFragment : Fragment() {
         val binding: FragmentInstructionBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_instruction, container, false
         )
+        binding.toShoeListBtn.setOnClickListener { view: View ->
+            view.findNavController().navigate(InstructionFragmentDirections.actionInstructionFragmentToShoeListFragment())
+        }
         return binding.root
     }
 }
