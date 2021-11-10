821bf18ca8980cfdb51dc974f3f504bf511074f8
Jason Force <jason.force@clickfunnels.com>
2021-11-09 22:29:04 -0600

    Add click listeners for cancel / save buttons

--- starter/app/src/main/java/com/udacity/shoestore/screens/shoes/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/shoes/ShoeDetailFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
@@ -18,6 +20,12 @@ class ShoeDetailFragment : Fragment(){
         val binding: FragmentShoeDetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_detail, container, false
         )
+        binding.shoeDetailCancelBtn.setOnClickListener { view: View ->
+            view.findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment())
+        }
+        binding.shoeDetailSaveBtn.setOnClickListener { view: View ->
+            Toast.makeText(this.context, "TODO: Save Shoe Detail", Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 }
